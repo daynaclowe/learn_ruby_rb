8cfc55c09dbcc158e0713e4edd9e841794ef9a4f
daynaclowe <daynalowe@Dayna-Lowes-MacBook-3.local>
2014-11-03 17:12:52 -0500

    02_calculator done

---
+++ 02_calculator/calculator.rb
@@ -0,0 +1,15 @@
+def add(x, y)
+	x + y
+end
+
+def subtract(x, y)
+	x - y
+end
+
+def sum(array)
+	array.reduce(:+) || 0  #if the first statement is false it will check the second statement - reduce will check the1st two values in the array and add them to create a new a value, then that new a will be added to the second value and so forth
+	# array.inject {|sum, n| sum + n } #inject is similar to reduce but it needs a starting value to get started. 
+end
+
+# array = Array.new
+# array = []
