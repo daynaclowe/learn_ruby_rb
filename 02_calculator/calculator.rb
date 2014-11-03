ae12cb03da3e96258a8872d3f6688c434e1e788e
daynaclowe <daynalowe@Dayna-Lowes-MacBook-3.local>
2014-11-03 17:58:44 -0500

    Added multiplication testing

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -8,8 +8,15 @@ end
 
 def sum(array)
 	array.reduce(:+) || 0  #if the first statement is false it will check the second statement - reduce will check the1st two values in the array and add them to create a new a value, then that new a will be added to the second value and so forth
-	# array.inject {|sum, n| sum + n } #inject is similar to reduce but it needs a starting value to get started. 
+	# array.inject {|sum, n| sum + n } #inject is similar to reduce but it needs a value in it's array to get started. 
 end
 
-# array = Array.new
-# array = []
+def multiply(array) #Question: do i need to put an array here?
+	array.inject(1) {|product, n| product * n}
+end
+
+def factorial(num)
+	if num <= 0
+		1
+end
+
