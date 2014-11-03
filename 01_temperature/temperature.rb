98d92e40aaaaa2a5cb5e0c14445a61c78e755682
daynaclowe <daynalowe@Dayna-Lowes-MacBook-3.local>
2014-11-03 16:20:50 -0500

    01_temperature done.

---
+++ 01_temperature/temperature.rb
@@ -0,0 +1,14 @@
+def ftoc(fahrenheit)
+	(fahrenheit - 32) * (5/9.0)
+end
+
+def ctof(celcius)
+	(celcius * 9) / 5 + 32
+end
+
+# Celcius: Multiply by 9, then divide by 5, then add 32
+
+# fahrenheit - 32 * (5/9) 
+
+# 1. One degree fahrenheit is 5/9 of one degree celsius
+# 2. The freezing point of water is 0 degrees celsius but 32 degrees fahr
